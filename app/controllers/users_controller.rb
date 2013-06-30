ea307e9863a6731f2a99dfe4bf52acf183ff7141
Kieran Coughlan <kcoughlan78@me.com>
2013-06-30 09:27:42 +0100

    deleted users files

--- app/controllers/users_controller.rb
+++
@@ -1,9 +0,0 @@
-class UsersController < ApplicationController
-  def index
-    @users = User.order("username")
-  end
-
-  def show
-    @user = !params[:email].nil? ? User.find(params[:email]) : current_user
-  end
-end
