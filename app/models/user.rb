ddf176c6b775e38110070f0289ca9b0f34192232
Kieran Coughlan <kcoughlan78@me.com>
2013-07-03 11:35:57 +0100

    added address2

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,15 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :bio, :address1
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :username, :bio, :address1, :address2
   # attr_accessible :title, :body
 
   validates_presence_of :username
 
   validates :address1, :presence => true,
             :length => { :maximum => 50 }
+
+  validates :address2, :length => { :maximum => 50 }
 
   validates :bio, :presence => true,
   :length => { :maximum => 300 }
