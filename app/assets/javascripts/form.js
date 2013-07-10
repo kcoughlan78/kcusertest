20db5cb6dc8847bd7dd5f9d9e8914e761aa0aca9
Kieran Coughlan <kcoughlan78@me.com>
2013-07-10 07:57:06 +0100

    test form logic

---
+++ app/assets/javascripts/form.js
@@ -0,0 +1,17 @@
+/**
+ * Created with JetBrains RubyMine.
+ * User: MacKE
+ * Date: 10/07/2013
+ * Time: 07:33
+ * To change this template use File | Settings | File Templates.
+ */
+var jobHunter = document.getElementById("jobHunter");
+
+$('#user_usertype_jobhunter').hover(function(){
+
+    $(jobHunter).css("visibility","visible");
+    $(jobHunter).css("height","auto");
+
+
+});
+
