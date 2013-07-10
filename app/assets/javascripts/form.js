3e94a2ab801b51e2a368b8ed55231fce2c44ca53
Kieran Coughlan <kcoughlan78@me.com>
2013-07-10 08:20:41 +0100

    added form.js to production precompile

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -5,9 +5,10 @@
  * Time: 07:33
  * To change this template use File | Settings | File Templates.
  */
+var JH_Input = document.getElementById("user_usertype_jobhunter");
 var jobHunter = document.getElementById("jobHunter");
 
-$('#user_usertype_jobhunter').hover(function(){
+$(JH_Input).click(function(){
 
     $(jobHunter).css("visibility","visible");
     $(jobHunter).css("height","auto");
