a6444a701cd23939388c890dd7deb7e0f360f0ec
Kieran Coughlan <kcoughlan78@me.com>
2013-07-11 09:07:18 +0100

    fixed form js

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -10,7 +10,28 @@ $(document).ready(function(){
 
 $(".radiobutton").click(function(){
     if ($('input[name=usertype]:checked').val() == "Jobhunter") {
-    $("#jobHunter").slideDown("fast");
+        $("#jobHunter").slideDown("fast");
+        $("#recruiters").slideUp("fast");
+        $("#smallBiz").slideUp("fast");
+        $("#hireBiz").slideUp("fast");
+    }
+    if ($('input[name=usertype]:checked').val() == "Freelancers & Small Businesses") {
+        $("#smallBiz").slideDown("fast");
+        $("#recruiters").slideUp("fast");
+        $("#jobHunter").slideUp("fast");
+        $("#hireBiz").slideUp("fast");
+    }
+    if ($('input[name=usertype]:checked').val() == "Hiring Business") {
+        $("#hireBiz").slideDown("fast");
+        $("#recruiters").slideUp("fast");
+        $("#jobHunter").slideUp("fast");
+        $("#smallBiz").slideUp("fast");
+    }
+    if ($('input[name=usertype]:checked').val() == "Recruitment Company") {
+        $("#recruiters").slideDown("fast");
+        $("#hireBiz").slideUp("fast");
+        $("#jobHunter").slideUp("fast");
+        $("#smallBiz").slideUp("fast");
     }
 });
 
