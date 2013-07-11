01c875d8eece448cc0e081fae96e40b881450b6a
Kieran Coughlan <kcoughlan78@me.com>
2013-07-11 20:54:31 +0100

    added more classes in an effort for greater form control added some details to improve user exp

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -12,25 +12,41 @@ jQuery(function($){
 $("input[type=radio]").click(function(){
     if ($('input[name=usertype]:checked').val() == "Jobhunter") {
         $(".jobHunter").slideDown("fast");
+        $(".commonFields").slideDown("fast");
+        $(".freeAcc").slideDown("fast");
+        $(".bizCommon").slideUp("fast");
+        $(".premium").slideUp("fast");
         $(".recruiters").slideUp("fast");
         $(".smallBiz").slideUp("fast");
         $(".hireBiz").slideUp("fast");
     }
     if ($('input[name=usertype]:checked').val() == "Freelancers & Small Businesses") {
         $(".smallBiz").slideDown("fast");
+        $(".commonFields").slideDown("fast");
+        $(".freeAcc").slideDown("fast");
+        $(".bizCommon").slideDown("fast");
+        $(".premium").slideUp("fast");
         $(".recruiters").slideUp("fast");
         $(".jobHunter").slideUp("fast");
         $(".hireBiz").slideUp("fast");
     }
     if ($('input[name=usertype]:checked').val() == "Hiring Business") {
         $(".hireBiz").slideDown("fast");
+        $(".commonFields").slideDown("fast");
+        $(".bizCommon").slideDown("fast");
+        $(".premium").slideDown("fast");
+        $(".freeAcc").slideUp("fast");
         $(".recruiters").slideUp("fast");
         $(".jobHunter").slideUp("fast");
         $(".smallBiz").slideUp("fast");
     }
     if ($('input[name=usertype]:checked').val() == "Recruitment Company") {
         $(".recruiters").slideDown("fast");
+        $(".commonFields").slideDown("fast");
+        $(".bizCommon").slideDown("fast");
+        $(".premium").slideDown("fast");
         $(".hireBiz").slideUp("fast");
+        $(".freeAcc").slideUp("fast");
         $(".jobHunter").slideUp("fast");
         $(".smallBiz").slideUp("fast");
     }
