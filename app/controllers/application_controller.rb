f38d6d881217b972ce6b10a085abe451a7a86a3f
Kieran Coughlan <kcoughlan78@me.com>
2013-07-13 18:30:42 +0100

    added bizcommon helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  def bizCommon
+  def biz_common
     if current_user and (current_user.usertype != "Jobhunter")
       return true
     end
   end
+
+  helper_method :biz_common
 
 
 end
