5584d4a3d0fb3737ca19eab8d8747098fbf85f86
Kieran Coughlan <kcoughlan78@me.com>
2013-07-15 14:58:05 +0100

    changed form to split values and displau options re-wrote form.js

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -9,48 +9,11 @@
 
 
 jQuery(function($){
-$("select[id=user_usertype]").click(function(){
-    if ($('option').val() == "Jobhunter") {
-        $(".jobHunter").slideDown("fast");
-        $(".commonFields").slideDown("fast");
-        $(".freeAcc").slideDown("fast");
-        $(".bizCommon").slideUp("fast");
-        $(".premium").slideUp("fast");
-        $(".recruiters").slideUp("fast");
-        $(".smallBiz").slideUp("fast");
-        $(".hireBiz").slideUp("fast");
-    }
-    if ($('input[name=usertype]:checked').val() == "Freelancers & Small Businesses") {
-        $(".smallBiz").slideDown("fast");
-        $(".commonFields").slideDown("fast");
-        $(".freeAcc").slideDown("fast");
-        $(".bizCommon").slideDown("fast");
-        $(".premium").slideUp("fast");
-        $(".recruiters").slideUp("fast");
-        $(".jobHunter").slideUp("fast");
-        $(".hireBiz").slideUp("fast");
-    }
-    if ($('input[name=usertype]:checked').val() == "Hiring Business") {
-        $(".hireBiz").slideDown("fast");
-        $(".commonFields").slideDown("fast");
-        $(".bizCommon").slideDown("fast");
-        $(".premium").slideDown("fast");
-        $(".freeAcc").slideUp("fast");
-        $(".recruiters").slideUp("fast");
-        $(".jobHunter").slideUp("fast");
-        $(".smallBiz").slideUp("fast");
-    }
-    if ($('input[name=usertype]:checked').val() == "Recruitment Company") {
-        $(".recruiters").slideDown("fast");
-        $(".commonFields").slideDown("fast");
-        $(".bizCommon").slideDown("fast");
-        $(".premium").slideDown("fast");
-        $(".hireBiz").slideUp("fast");
-        $(".freeAcc").slideUp("fast");
-        $(".jobHunter").slideUp("fast");
-        $(".smallBiz").slideUp("fast");
-    }
-});
+    $('#user_usertype').change(function(){
+        var selected = $(this).find(':selected');
+        $('.reg').hide();
+        $('.'+selected.val()).show();
+    });
 });
 
 
